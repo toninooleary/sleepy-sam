3b457d16b71946a4447497964513047e9ccba48b
toninooleary <toninooleary@gmail.com>
2020-08-17 01:08:35 +0100

    Export to new project
    Old project assets and dependencies have been exported to the new project version.

---
+++ sleepy_sam_project_lts/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public Transform[] spawningPoints;
+
+    public GameObject obstacle;
+    private GameObject obstacleSpawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, spawningPoints.Length);
+
+
+        for (int i = 0; i < spawningPoints.Length; i++){
+            if (rand != i){
+                obstacleSpawned = Instantiate(obstacle, spawningPoints[i].position, Quaternion.identity);
+                obstacleSpawned.transform.SetParent(gameObject.transform);
+           }
+       }
+    }
+}
---
+++ sleepy_sam_project_lts/Assets/Scripts/cameraShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraShake : MonoBehaviour
+{
+    public IEnumerator Shake(float durationTime, float shakeStrength){
+        
+        Vector3 originalPosition = transform.localPosition;
+        
+        float timePassed = 0.0f;
+
+        while (timePassed < durationTime){
+            float x = Random.Range(-1f, 1f) * shakeStrength;
+            float y = Random.Range(-1f, 1f) * shakeStrength;
+
+            transform.localPosition = new Vector3(x,y, originalPosition.z);
+
+            //increases it by the time that has passed by
+            timePassed += Time.deltaTime;
+
+            //waits for next frame to start
+            yield return null;
+        }
+
+        //resets the camera position
+        transform.localPosition = originalPosition;
+    }
+}
