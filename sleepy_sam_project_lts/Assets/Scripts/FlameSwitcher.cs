0ef4536f3dad3f5d623ab680ec33f3d407b50ed9
toninooleary <toninooleary@gmail.com>
2020-08-30 10:41:22 +0100

    Create cooker stage
    Cooker stage has been created along with a script to switch cooker flames from side to side over time. This was tested in a scene created called "CookerStageTest"

---
+++ sleepy_sam_project_lts/Assets/Scripts/FlameSwitcher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameSwitcher : MonoBehaviour
+{
+    String currentSide;
+    private float timer;
+    GameObject spawnTL;
+    GameObject spawnBL;
+    GameObject spawnTR;
+    GameObject spawnBR;
+    GameObject bottomFlame;
+    GameObject topFlame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int startingSide = UnityEngine.Random.Range(0, 2);
+        bottomFlame = GameObject.Find("BottomFlameParticles");
+        topFlame = GameObject.Find("TopFlameParticles");
+        spawnTL = GameObject.Find("FlameSpawnPointTL");
+        spawnBL = GameObject.Find("FlameSpawnPointBL");
+        spawnTR = GameObject.Find("FlameSpawnPointTR");
+        spawnBR = GameObject.Find("FlameSpawnPointBR");
+        //spawns flames on the left
+        if (startingSide == 0)
+        {
+            bottomFlame.transform.position = spawnBL.transform.position;
+            topFlame.transform.position = spawnTL.transform.position;
+            currentSide = "left";
+        }
+        // spawns flames on the right
+        else 
+        {
+            bottomFlame.transform.position = spawnBR.transform.position;
+            topFlame.transform.position = spawnTR.transform.position;
+            currentSide = "right";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        Debug.Log(UnityEngine.Random.Range(0, 2));
+        // switches flame to right side
+        if (timer >= 5) {
+            if (currentSide == "left")
+            {
+                bottomFlame.transform.position = spawnBR.transform.position;
+                topFlame.transform.position = spawnTR.transform.position;
+                bottomFlame.GetComponent<ParticleSystem>().Play();
+                topFlame.GetComponent<ParticleSystem>().Play();
+                timer = 0;
+                currentSide = "right";
+            }
+            // switches flame to left side
+            else
+            {
+                bottomFlame.transform.position = spawnBL.transform.position;
+                topFlame.transform.position = spawnTL.transform.position;
+                bottomFlame.GetComponent<ParticleSystem>().Play();
+                topFlame.GetComponent<ParticleSystem>().Play();
+                timer = 0;
+                currentSide = "left";
+            }
+        }
+    }
+}
