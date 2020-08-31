5091059a3e6a05b5a45676cb7771ff3b2cdfba15
toninooleary <toninooleary@gmail.com>
2020-08-31 01:46:49 +0100

    Clean up FlameSwitcher.cs
    Code has been cleaned up

--- sleepy_sam_project_lts/Assets/Scripts/FlameSwitcher.cs
+++ sleepy_sam_project_lts/Assets/Scripts/FlameSwitcher.cs
@@ -5,68 +5,57 @@ using UnityEngine;
 
 public class FlameSwitcher : MonoBehaviour
 {
-    String currentSide;
-    private float timer;
     GameObject spawnTL;
     GameObject spawnBL;
     GameObject spawnTR;
     GameObject spawnBR;
     GameObject bottomFlame;
     GameObject topFlame;
+    int currentSide;
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        int startingSide = UnityEngine.Random.Range(0, 2);
         bottomFlame = GameObject.Find("BottomFlameParticles");
         topFlame = GameObject.Find("TopFlameParticles");
         spawnTL = GameObject.Find("FlameSpawnPointTL");
         spawnBL = GameObject.Find("FlameSpawnPointBL");
         spawnTR = GameObject.Find("FlameSpawnPointTR");
         spawnBR = GameObject.Find("FlameSpawnPointBR");
-        //spawns flames on the left
-        if (startingSide == 0)
-        {
-            bottomFlame.transform.position = spawnBL.transform.position;
-            topFlame.transform.position = spawnTL.transform.position;
-            currentSide = "left";
-        }
-        // spawns flames on the right
-        else 
-        {
-            bottomFlame.transform.position = spawnBR.transform.position;
-            topFlame.transform.position = spawnTR.transform.position;
-            currentSide = "right";
-        }
+        //spawns flames on the left or tight
+        int currentSide = UnityEngine.Random.Range(0, 2);
+        Debug.Log(currentSide);
+        setSide();
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        Debug.Log(timer);
-        Debug.Log(UnityEngine.Random.Range(0, 2));
-        // switches flame to right side
         if (timer >= 5) {
-            if (currentSide == "left")
-            {
-                bottomFlame.transform.position = spawnBR.transform.position;
-                topFlame.transform.position = spawnTR.transform.position;
-                bottomFlame.GetComponent<ParticleSystem>().Play();
-                topFlame.GetComponent<ParticleSystem>().Play();
-                timer = 0;
-                currentSide = "right";
-            }
-            // switches flame to left side
-            else
-            {
-                bottomFlame.transform.position = spawnBL.transform.position;
-                topFlame.transform.position = spawnTL.transform.position;
-                bottomFlame.GetComponent<ParticleSystem>().Play();
-                topFlame.GetComponent<ParticleSystem>().Play();
-                timer = 0;
-                currentSide = "left";
-            }
+            // using the XOR operator to switch sides
+            currentSide = currentSide ^ 1;
+            setSide();
+            bottomFlame.GetComponent<ParticleSystem>().Play();
+            topFlame.GetComponent<ParticleSystem>().Play();
+            timer = 0;
+        }
+    }
+
+    // called to move the flame to the left or right of the stage
+    void setSide() {
+        // moves flames to left side
+        if (currentSide == 0)
+        {
+            bottomFlame.transform.position = spawnBL.transform.position;
+            topFlame.transform.position = spawnTL.transform.position;
+        }
+        // moves flames to right side
+        else
+        {
+            bottomFlame.transform.position = spawnBR.transform.position;
+            topFlame.transform.position = spawnTR.transform.position;
         }
     }
 }
