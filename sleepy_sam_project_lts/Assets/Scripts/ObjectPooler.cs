b856dc3cff07b7b478eea571ad4a4b36d5285da2
toninooleary <toninooleary@gmail.com>
2020-09-18 23:07:47 +0100

    Finish tile assets for bathroom
    tile variations for the bathroom stage have been created. They also had textures created.

---
+++ sleepy_sam_project_lts/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool 
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool p in pools) 
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < p.size; i++)
+            {
+                GameObject obj = Instantiate(p.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(p.tag, objectPool);
+        }
+    }
+
+    public GameObject spawnFromPool(string tag, Vector3 pos, Quaternion rotation) {
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = pos;
+        objectToSpawn.transform.rotation = rotation;
+        poolDictionary.[tag].Enqueue(objectToSpawn);
+        return objectToSpawn;
+    }
+
+    public void 
+
+}
---
+++ sleepy_sam_project_lts/Assets/Scripts/WindowSpawnerUpdated.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class WindowSpawnerUpdated : MonoBehaviour
+{
+
+    public Transform[] spawningPoints;
+
+    public GameObject window;
+    private GameObject windowSpawned;
+    public int numOfWindows;
+    public int minNumOfWindows;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        for (int i = 0; i <= spawningPoints.Length; i++)
+        {
+            int spawnProbability = Random.Range(0, 10);
+            if (spawnProbability <= 3)
+            {
+                windowSpawned = Instantiate(window, spawningPoints[i].position, window.transform.rotation);
+                windowSpawned.transform.SetParent(gameObject.transform);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
