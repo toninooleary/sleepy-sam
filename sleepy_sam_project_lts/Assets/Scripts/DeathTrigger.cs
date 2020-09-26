1c4027adc0da1f93763a14074c2a54ad05d667a7
toninooleary <toninooleary@gmail.com>
2020-09-26 13:44:10 +0100

    Fix CookerStage
    Cooker stage has been fixed and now works properely as a "stage". Camera shake and player death weren't working.

---
+++ sleepy_sam_project_lts/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+    private DeathCameraShake shaker;
+    public GameObject deathParticles;
+
+    void Start()
+    {
+        shaker = GameObject.Find("CollisionShakeManager").GetComponent<DeathCameraShake>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Colliding>() != null)
+        {
+            other.GetComponent<Colliding>().enabled = false;
+        }
+
+        if (other.GetComponent<RagdollMovement>() != null)
+        {
+            if (other.GetComponent<RagdollMovement>().enabled == true) 
+            {
+                other.GetComponent<RagdollMovement>().enabled = false;
+                StartCoroutine(shaker.Shake(0.13f, 0.25f));
+                Instantiate(deathParticles, transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
